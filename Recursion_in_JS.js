3befb7e25ab7fbe57c4590420c7fcbc8f2cdd204
yandong liu <askjupiter@gmail.com>
2014-06-09 14:49:00 -0700

    g

---
+++ Recursion_in_JS.js
@@ -0,0 +1,21 @@
+//https://www.cs.cmu.edu/~yandongl/recursion_in_js.html
+function findParent(path, db, child, cb) {
+  db.find({children:{$elemMatch:{id:child}}}).toArray(function (err, items) {
+    var rs = [];
+    if( items.length==0 ) {
+      cb('no parent', path.slice(0));
+    } else {
+      async.eachSeries(items, function(e, cb1) {
+        path.push(e.id);
+        findParent(path, db, e.id, function(rs1){
+          rs = rs.concat(rs1);
+          path.pop();
+          cb1();
+        });
+      }, function(err){
+        if(err) console.log('async error.');
+        cb(err, rs);
+      });  
+    }
+  });
+}
